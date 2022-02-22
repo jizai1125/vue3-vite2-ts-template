2c89a5b6a63391d6e43d523fd8ec19ae65739f55
jizai1125 <1414589221@qq.com>
2022-02-22 15:00:48 +0800

    feat: 添加缩放适配插件

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import router from './router'
 import { createPinia } from 'pinia'
 // svg 图标注册 vite-plugin-svg-icons
 import 'virtual:svg-icons-register'
+import ScreenFlexible from './plugins/screen-flexible'
 
 const app = createApp(App)
+
+app.use(ScreenFlexible, { el: '#app', remWidth: 3840, remHeight: 2160 })
 
 app.use(router)
 app.use(createPinia())
